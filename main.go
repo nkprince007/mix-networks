6513fd36b396a37ab9e4253b033a31a6863cab55
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2020-03-20 13:23:43 -0500

    main.go: Usage example for ThresholdMix

--- main.go
+++ main.go
@@ -1,34 +1,68 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"math/rand"
 	"os"
 	"os/signal"
 	"time"
 
-	"github.com/nkprince007/mix-networks/server"
+	"github.com/nkprince007/mix-networks/mixes"
 )
 
-func main() {
-	done := make(chan os.Signal, 1)
-	go signal.Notify(done, os.Interrupt, os.Kill)
-
-	srv := server.Server{
-		Addr:            ":8080",
-		IdleConnTimeout: time.Second * 15,
-		BufferSize:      1024,
+func feeder(m mixes.Mix) {
+	for {
+		time.Sleep(time.Duration(rand.Intn(5)*100) * time.Millisecond)
+		msg := mixes.Message(rand.Int())
+		m.AddMessage(msg)
 	}
-
-	go func() {
-		err := srv.ListenAndServe()
-		if err != nil {
-			log.Fatalf("Server error on %v, %v\n", srv.Addr, err)
-		}
-	}()
-
-	<-done
-	if err := srv.ShutDown(); err != nil {
-		log.Fatalf("Shutdown error: %v\n", err)
-	}
-	os.Exit(0)
 }
+
+func listenToSignals(done chan interface{}) {
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, os.Interrupt, os.Kill)
+	fmt.Println("signal received: ", <-s)
+	fmt.Println("shutting down...")
+	done <- nil
+}
+
+func main() {
+	done := make(chan interface{}, 1)
+	go listenToSignals(done)
+
+	var mix mixes.Mix = &mixes.ThresholdMix{Size: 4}
+	go feeder(mix)
+	for {
+		select {
+		case <-done:
+			mix.CleanUp()
+			return
+		default:
+			fmt.Println(mix.GetMessages())
+		}
+	}
+}
+
+// func main() {
+// 	done := make(chan os.Signal, 1)
+// 	go signal.Notify(done, os.Interrupt, os.Kill)
+
+// 	srv := server.Server{
+// 		Addr:            ":8080",
+// 		IdleConnTimeout: time.Second * 15,
+// 		BufferSize:      1024,
+// 	}
+
+// 	go func() {
+// 		err := srv.ListenAndServe()
+// 		if err != nil {
+// 			log.Fatalf("Server error on %v, %v\n", srv.Addr, err)
+// 		}
+// 	}()
+
+// 	<-done
+// 	if err := srv.ShutDown(); err != nil {
+// 		log.Fatalf("Shutdown error: %v\n", err)
+// 	}
+// 	os.Exit(0)
+// }
